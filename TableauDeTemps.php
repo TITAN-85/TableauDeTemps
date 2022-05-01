fb3a97a596fa1492e6ba2795897966020160e082
TITAN-85 <102234554+TITAN-85@users.noreply.github.com>
2022-05-01 11:52:51 -0400

    Update TableauDeTemps.php

--- TableauDeTemps.php
+++ TableauDeTemps.php
@@ -7,12 +7,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de Temps</title>
     <style>
+        body {
+            width: 60%;
+            margin: auto;
+        }
+
+        table, th, td {
+            border: 3px solid grey;
+            border-collapse: collapse;
+        }
+
+        th {
+            background-color: darkseagreen;
+            color: red;
+        }
+
+        th, td {
+            width: 55px;
+            line-height: 40px;
+            text-align: center;
+        }
+
         td {
-            background-color: grey;
-            padding: 10px;
+            color: green;
         }
     </style>
 </head>
+
 <body>
     <nav>
         <a href="Travail-Pratique-3.html"></a>
@@ -24,36 +45,42 @@
             $d = date('Y-m-d:i:s');
             ?>
         </section>
-        <h1>Tableau de <?php echo date("h:i"); ?> </h1>
-        <?php
-        $timeBoard = [];
-        $Hour = date("h");
-        $minutes = date("i");
+        <section>
+            <h1>Tableau de Temp<?php echo date("h:i"); ?> </h1>
+            <?php
+            $timeBoard = [];
+            $Hour = date("h");
+            $minutes = date("i");
 
-
-        for ($i = 0; $i < 60; $i++) {
-            $timeBoard[] = [];
-            for ($j = 0; $j < 12; $j++) {
-                $timeBoard[$i][] = $i . " " . $j;
+            for ($i = 0; $i < 60; $i++) {
+                $timeBoard[] = [];
+                for ($j = 0; $j < 12; $j++) {
+                    $timeBoard[$i][] = $i . " " . $j;
+                }
             }
-        }
-        ?> <td> " " </td> <?php [$timeBoard];?>
-        <td> <?php ?> </td>
-        <?php
 
-        ?>
+            ?>
+        </section>
         <table>
             <tr>
-                <?php for ($i = 0; $i < 20; $i++) {
-                    $a[$i] = " x ";
-                ?>
-                    <td><?php echo $i; ?></td>
-                <?php } ?>
+                <th>Ligne/Colones</th>
+                <?php for ($i = 0; $i < 12; $i++) : ?>
+                    <th><?= $i ?></th>
+                <?php endfor ?>
             </tr>
+            <?php for( $i =0; $i < 60; $i++) : ?>
             <tr>
-                <?php //echo 
-                ?> <td></td> <?php ?>
+            <th><?= $i ?></th>
+            <?php for( $j =0; $j < 12; $j++) : ?>
+                <td><?= 
+
+            $timeBoard[$Hour][$minutes] = "X";
+            $timeBoard[$i][$j];
+                ?></td>
+
+        <?php endfor ?>
             </tr>
+            <?php endfor ?>      
         </table>
     </main>
 </body>
